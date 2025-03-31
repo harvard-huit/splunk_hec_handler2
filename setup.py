beb4cfef77e255fea4203501add0d97e3427a607
Matthew Woodward <MrWoodward@users.noreply.github.com>
2025-03-31 16:24:36 -0400

    v1.3.0 -- Combined best features of SplunkHttpHandler into SplunkHecHandler

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 setup(
     name='splunk_hec_handler',
-    version='1.2.0',
+    version='1.3.0',
     license='MIT License',
     description='A Python logging handler to sends logs to Splunk using HTTP event collector (HEC)',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='vavarachen',
-    author_email='vavarachen@gmail.com',
     url='https://github.com/vavarachen/splunk_hec_handler',
     packages=['splunk_hec_handler'],
+    python_requires='>=3.6',
     install_requires=['requests >= 2.6.0, < 3.0.0', 'requests'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -21,12 +21,13 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: System :: Logging'
     ]
 )
